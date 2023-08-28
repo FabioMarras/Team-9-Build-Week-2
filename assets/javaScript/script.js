cecb7597d113f7861748874d5778897262f4d948
Bilgen Kaan Remzi <137759282+Rekabil@users.noreply.github.com>
2023-08-28 13:06:20 +0200

    SideBar

---
+++ assets/javaScript/script.js
@@ -0,0 +1,34 @@
+function collapseRight(event) {
+  const int = document.querySelector(".internal");
+  int.style.display = "none";
+  document.querySelector(".expand-right").style.display = "block";
+  const sidebar = document.querySelector(".sidebarRight");
+  console.log(sidebar);
+  sidebar.classList.toggle("collapsed");
+}
+function expandRight(event) {
+  document.querySelector(".internal").style.display = "flex";
+  document.querySelector(".expand-right").style.display = "none";
+  const sidebar = document.querySelector(".sidebarRight");
+  console.log(sidebar);
+  sidebar.classList.toggle("collapsed");
+}
+
+function collapseLeft(event) {
+  const disp = document.querySelector(".sidebarLeft").querySelectorAll("span");
+  disp.forEach((element) => {
+    element.style.display = "none";
+  });
+  document.querySelector(".sidebarLeft").classList.toggle("collapsed");
+  document.querySelector(".expand-left").classList.toggle("d-none");
+  document.querySelector(".collapse-left").classList.toggle("d-none");
+}
+function expandLeft(event) {
+  const disp = document.querySelector(".sidebarLeft").querySelectorAll("span");
+  disp.forEach((element) => {
+    element.style.display = "inline";
+  });
+  document.querySelector(".expand-left").classList.toggle("d-none");
+  document.querySelector(".collapse-left").classList.toggle("d-none");
+  document.querySelector(".sidebarLeft").classList.toggle("collapsed");
+}
