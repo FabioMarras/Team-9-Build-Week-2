66183cca381aa7f1e772e9ee33db41d9c21f7ea8
Bilgen Kaan Remzi <137759282+Rekabil@users.noreply.github.com>
2023-08-30 12:07:22 +0200

    searchbR

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -176,83 +176,7 @@ function artistPage() {
 const URL = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
 const URL2 = "https://striveschool-api.herokuapp.com/api/deezer/search?q={query}";
 
-/* window.onload = () => {
-  getArtisits();
-}; */
-
-//const getArtisits = async () => {
-/*   try {
-    const resp = await fetch(URL, {
-      method: 'GET',
-      headers: {
-        Authorization: '',
-      },
-    });
-
-    if (resp.ok) {
-      const responseData = await resp.json(); */
-//console.log("responde data: ", responseData)
-
-/*  try {
-        const resp = await fetch(URL, {
-          method: 'GET',
-          headers: {
-            Authorization: '*',
-          },
-        }); */
-
-/*    if (resp.ok) {
-          const responseData = await resp.json();
-          console.log('responde data: ', responseData);
-
-          const row = document.getElementById('row');
-          const trackList = fetch(responseData.data[0].album.tracklist);
-
-          console.log(trackList); */
-
-/*  trackList.data.forEach((data) => {
-            console.log(data); */
-/*const col = document.createElement("div");
-            col.innerHTML = `
-            <p> ${data.album.title} </p>
-            `;
-            console.log(data.album.title);
-    
-            row.appendChild(col);*/
-/* const song1 = document.getElementById('titleSong');
-            const titleSongAlbum = document.createElement('div');
-            titleSongAlbum.innerHTML = `
-            <div class="row text-center mb-3 justify-content-between">
-                <div class="col-1">
-                  <span class="text-light">1</span>
-                </div>
-                <div class="col-6" id="titleSong">
-                  <div class="row text-light">${data.title}</div>
-                  <div class="row text-white-50">Pinguini</div>
-                </div>
-                <div class="col-4 text-start text-white-50">694,578</div>
-                <div class="col-1">
-                  <span class="text-white-50">1:28</span>
-                </div>
-              </div>
-            <div class="row text-light">${data.album.tracklist}</div>
-              `; */
-/* song1.appendChild(titleSongAlbum);
-          });
-        } else {
-          console.error('Errore nella richiesta:', resp.status);
-        }
-      } catch (error) {}
-    }
-  } catch (error) {}
-}; */
-
-/* ------------- */
-
-//const artistName = new URLSearchParams(Window.location.search).get('artistName');
-//console.log('id');
-
-window.onload = () => {
+function searchbar() {
   const searchButton = document.getElementById("button-addon1"); //search button
   const searchResults = document.getElementById("searchResults"); //empty div within search button
   searchButton.addEventListener("click", () => {
@@ -263,14 +187,8 @@ window.onload = () => {
     fetch(URL + searchQuery)
       .then((resp) => resp.json())
       .then((artistObjs) => {
-        //console.log(artistObjs);
         artistObjs.data.forEach((artistObj) => {
-          //console.log(artistObjs.data);
-          //let artistCard = document.createElement('div');
-
-          searchResults.innerHTML += `  
-         
-          <div class="row g-1 my-3 d-flex">
+          searchResults.innerHTML += `<div class="row g-1 my-3 d-flex">
           <div class="col-md-3"> 
           <a href="${artistObj.link}"><img src="${artistObj.album.cover_big}" class="img-fluid rounded-start h-100" alt="..."></a>
           </div> 
@@ -278,21 +196,8 @@ window.onload = () => {
           <div class="card-body">
           <h5 class="card-title">Title: ${artistObj.title}</h5>
           <p class="card-text"><a href="../Artist.html?artistId=${artistObj.artist.id}">Artist: ${artistObj.artist.name}</a></p>
-          <p class="card-text"><a href="../album.html?albumId=${artistObj.album.id}">Album: ${artistObj.album.title}</a></p>
-          </div>
-          </div>
-          </div>
-          
-          `;
-
-          //console.log(artistCard);
-          //console.log(searchResults);
-
-          //searchResults.appendChild(artistCard);
+          <p class="card-text"><a href="../album.html?albumId=${artistObj.album.id}">Album: ${artistObj.album.title}</a></p></div></div></div> `;
         });
       });
   });
-};
-
-//const eventId = new URLSearchParams(Window.location.search).get('getId');
-//console.log('id');
+}
