ea602cb5cdaa65804f645d8775f7f238749e7284
Godwin <godwinmail6@gmail.com>
2023-08-31 12:57:12 +0200

    like button

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,49 +1,49 @@
 function collapseRight(event) {
-  const int = document.querySelector(".internal");
-  int.style.display = "none";
-  document.querySelector(".expand-right").style.display = "block";
-  const sidebar = document.querySelector(".sidebarRight");
-  sidebar.classList.toggle("collapsed");
-  document.getElementById("main").classList.toggle("pSidebarright");
+  const int = document.querySelector('.internal');
+  int.style.display = 'none';
+  document.querySelector('.expand-right').style.display = 'block';
+  const sidebar = document.querySelector('.sidebarRight');
+  sidebar.classList.toggle('collapsed');
+  document.getElementById('main').classList.toggle('pSidebarright');
 }
 function expandRight(event) {
-  document.querySelector(".internal").style.display = "flex";
-  document.querySelector(".expand-right").style.display = "none";
-  const sidebar = document.querySelector(".sidebarRight");
-  sidebar.classList.toggle("collapsed");
-  document.getElementById("main").classList.toggle("pSidebarright");
+  document.querySelector('.internal').style.display = 'flex';
+  document.querySelector('.expand-right').style.display = 'none';
+  const sidebar = document.querySelector('.sidebarRight');
+  sidebar.classList.toggle('collapsed');
+  document.getElementById('main').classList.toggle('pSidebarright');
 }
 
 function collapseLeft(event) {
-  const disp = document.querySelector(".sidebarLeft").querySelectorAll("span");
+  const disp = document.querySelector('.sidebarLeft').querySelectorAll('span');
   disp.forEach((element) => {
-    element.style.display = "none";
+    element.style.display = 'none';
   });
-  document.querySelector(".sidebarLeft").classList.toggle("collapsed");
-  document.querySelector(".expand-left").classList.toggle("d-none");
-  document.querySelector(".collapse-left").classList.toggle("d-none");
-  document.getElementById("main").classList.toggle("pSidebarleft");
+  document.querySelector('.sidebarLeft').classList.toggle('collapsed');
+  document.querySelector('.expand-left').classList.toggle('d-none');
+  document.querySelector('.collapse-left').classList.toggle('d-none');
+  document.getElementById('main').classList.toggle('pSidebarleft');
 }
 function expandLeft(event) {
-  const disp = document.querySelector(".sidebarLeft").querySelectorAll("span");
+  const disp = document.querySelector('.sidebarLeft').querySelectorAll('span');
   disp.forEach((element) => {
-    element.style.display = "inline";
+    element.style.display = 'inline';
   });
-  document.querySelector(".expand-left").classList.toggle("d-none");
-  document.querySelector(".collapse-left").classList.toggle("d-none");
-  document.querySelector(".sidebarLeft").classList.toggle("collapsed");
-  document.getElementById("main").classList.toggle("pSidebarleft");
+  document.querySelector('.expand-left').classList.toggle('d-none');
+  document.querySelector('.collapse-left').classList.toggle('d-none');
+  document.querySelector('.sidebarLeft').classList.toggle('collapsed');
+  document.getElementById('main').classList.toggle('pSidebarleft');
 }
 function appearSearch(event) {
-  document.querySelector(".searchBar").classList.toggle("d-none");
-  document.querySelector(".mainPage").classList.toggle("d-none");
+  document.querySelector('.searchBar').classList.toggle('d-none');
+  document.querySelector('.mainPage').classList.toggle('d-none');
 }
 
 const options = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    "X-RapidAPI-Key": "c47f8709a8msh68fc06fa8886e81p18e74djsn87e3ff301cd9",
-    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+    'X-RapidAPI-Key': 'c47f8709a8msh68fc06fa8886e81p18e74djsn87e3ff301cd9',
+    'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
   },
 };
 
@@ -51,47 +51,47 @@ const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
 console.log(urlParams);
-const urlAlbum = "https://deezerdevs-deezer.p.rapidapi.com/album/";
-const albumId = urlParams.get("albumId");
+const urlAlbum = 'https://deezerdevs-deezer.p.rapidapi.com/album/';
+const albumId = urlParams.get('albumId');
 
 console.log(albumId);
-const url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=103248";
+const url = 'https://deezerdevs-deezer.p.rapidapi.com/search?q=103248';
 function album() {
   fetch(urlAlbum + albumId, options)
     .then((responseObj) => responseObj.json())
     .then((obj) => {
-      const mList = document.getElementById("musicList");
+      const mList = document.getElementById('musicList');
       const trackList = obj.tracks;
       let i = 1;
       const albumTime = obj.duration;
       const albumMinutes = Math.floor(albumTime / 60);
       const albumSeconds = Math.floor(albumTime - Math.floor(albumTime / 60) * 60)
         .toString()
-        .padStart(2, "0");
-      const div = document.getElementById("musicList");
-      document.getElementById("topImage").src = `${obj.cover}`;
-      document.querySelector(".albumTitle").innerHTML = `${obj.title}`;
-      document.querySelector(".albumArtist").innerHTML = `${obj.artist.name}`;
-      document.querySelector("#artistProfileImage").src = `${obj.artist.picture}`;
-      document.querySelector(".releaseDate").innerHTML = `${obj.release_date}`;
-      document.querySelector(".albumDuration").innerHTML = `${
-        " " + albumMinutes + " minutes " + albumSeconds + " seconds"
+        .padStart(2, '0');
+      const div = document.getElementById('musicList');
+      document.getElementById('topImage').src = `${obj.cover}`;
+      document.querySelector('.albumTitle').innerHTML = `${obj.title}`;
+      document.querySelector('.albumArtist').innerHTML = `${obj.artist.name}`;
+      document.querySelector('#artistProfileImage').src = `${obj.artist.picture}`;
+      document.querySelector('.releaseDate').innerHTML = `${obj.release_date}`;
+      document.querySelector('.albumDuration').innerHTML = `${
+        ' ' + albumMinutes + ' minutes ' + albumSeconds + ' seconds'
       }`;
-      document.querySelector(".nBrani").innerHTML = `${" " + trackList.length + " Brani " + " , "}`;
+      document.querySelector('.nBrani').innerHTML = `${' ' + trackList.length + ' Brani ' + ' , '}`;
 
       trackList.data.forEach((obj) => {
-        const row = document.createElement("div");
-        row.classList.add("row");
-        row.classList.add("text-center");
-        row.classList.add("mb-3");
-        row.classList.add("justify-content-between");
+        const row = document.createElement('div');
+        row.classList.add('row');
+        row.classList.add('text-center');
+        row.classList.add('mb-3');
+        row.classList.add('justify-content-between');
         const time = obj.duration;
         const minutes = Math.floor(time / 60);
         const seconds = Math.floor(time - Math.floor(time / 60) * 60)
           .toString()
-          .padStart(2, "0");
+          .padStart(2, '0');
 
-        const duration = minutes + ":" + seconds;
+        const duration = minutes + ':' + seconds;
 
         row.innerHTML = `<div class="col-1">
             <span class="text-light">${i}</span>
@@ -110,35 +110,35 @@ function album() {
     });
 }
 
-const generalAPI = "https://striveschool-api.herokuapp.com/api/deezer/search?q={query}";
+const generalAPI = 'https://striveschool-api.herokuapp.com/api/deezer/search?q={query}';
 
-const eventI = new URLSearchParams(window.location.search).get("artistId");
-const urlArtist = "https://striveschool-api.herokuapp.com/api/deezer/artist/";
+const eventI = new URLSearchParams(window.location.search).get('artistId');
+const urlArtist = 'https://striveschool-api.herokuapp.com/api/deezer/artist/';
 
 function artistPage() {
-  const url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem";
+  const url = 'https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem';
   const options = {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "X-RapidAPI-Key": "c47f8709a8msh68fc06fa8886e81p18e74djsn87e3ff301cd9",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+      'X-RapidAPI-Key': 'c47f8709a8msh68fc06fa8886e81p18e74djsn87e3ff301cd9',
+      'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
     },
   };
   fetch(urlArtist + eventI, options)
     .then((responseObj) => responseObj.json())
     .then((obj) => {
-      const div = document.getElementById("musicList");
-      const nameArtist = document.getElementById("nameArtist");
-      const nameArt = document.createElement("div");
-      document.getElementById("artistCoverImg").style.backgroundImage = `url(${obj.picture_big})`;
+      const div = document.getElementById('musicList');
+      const nameArtist = document.getElementById('nameArtist');
+      const nameArt = document.createElement('div');
+      document.getElementById('artistCoverImg').style.backgroundImage = `url(${obj.picture_big})`;
       nameArt.innerHTML = `
 
   <h1> ${obj.name} </h1>
   `;
       nameArtist.appendChild(nameArt);
 
-      const songListPopolar = document.getElementById("songListPopolar");
-      fetch(urlArtist + eventI + "/top?limit=50", options)
+      const songListPopolar = document.getElementById('songListPopolar');
+      fetch(urlArtist + eventI + '/top?limit=50', options)
         .then((responseObj) => responseObj.json())
         .then((obj) => {
           for (let i = 0; i < 5; i++) {
@@ -146,11 +146,11 @@ function artistPage() {
             const minutes = Math.floor(time / 60);
             const seconds = Math.floor(time - Math.floor(time / 60) * 60)
               .toString()
-              .padStart(2, "0");
+              .padStart(2, '0');
 
-            const duration = minutes + ":" + seconds;
-            const listPopolar = document.createElement("div");
-            listPopolar.className = "d-flex mt-3";
+            const duration = minutes + ':' + seconds;
+            const listPopolar = document.createElement('div');
+            listPopolar.className = 'd-flex mt-3';
             listPopolar.innerHTML = `
             <div class="d-flex mt-3" style="width: 100%;">
             <span class="mx-3 align-self-center" style="width: 2%;">${i + 1}</span>
@@ -166,11 +166,11 @@ function artistPage() {
     });
 }
 
-const URL = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
-const URL2 = "https://striveschool-api.herokuapp.com/api/deezer/search?q={query}";
+const URL = 'https://striveschool-api.herokuapp.com/api/deezer/search?q=';
+const URL2 = 'https://striveschool-api.herokuapp.com/api/deezer/search?q={query}';
 
 const getData = (searchQuery) => {
-  searchResults.innerHTML = "";
+  searchResults.innerHTML = '';
 
   fetch(URL + searchQuery)
     .then((resp) => resp.json())
@@ -190,77 +190,96 @@ const getData = (searchQuery) => {
 };
 
 function searchbarAndPlaylist() {
-  const searchInput = document.getElementById("search-input");
-  const searchResults = document.getElementById("searchResults");
-  const playlistDiv = document.getElementById("playlist");
-  searchInput.addEventListener("input", (event) => {
+  const searchInput = document.getElementById('search-input');
+  const searchResults = document.getElementById('searchResults');
+  const playlistDiv = document.getElementById('playlist');
+  searchInput.addEventListener('input', (event) => {
     getData(event.target.value);
   });
 }
 
-const song = document.getElementById("song");
+const song = document.getElementById('song');
 
 function player(event) {
-  const trackUrl = "https://striveschool-api.herokuapp.com/api/deezer/track/";
+  const trackUrl = 'https://striveschool-api.herokuapp.com/api/deezer/track/';
 
-  localStorage.setItem("lastSong", event);
+  localStorage.setItem('lastSong', event);
 
   fetch(trackUrl + event)
     .then((resp) => resp.json())
     .then((trackObj) => {
       console.log(trackObj);
-      document.querySelector(".playerIMG").src = `${trackObj.album.cover}`;
-      document.querySelector(".playerName").innerHTML = `${trackObj.title_short}`;
-      document.querySelector(".playerArtist").innerHTML = `${trackObj.artist.name}`;
-      document.getElementById("songSrc").src = `${trackObj.preview}`;
+      document.querySelector('.playerIMG').src = `${trackObj.album.cover}`;
+      document.querySelector('.playerName').innerHTML = `${trackObj.title_short}`;
+      document.querySelector('.playerArtist').innerHTML = `${trackObj.artist.name}`;
+      document.getElementById('songSrc').src = `${trackObj.preview}`;
       const time = trackObj.duration;
       const minutes = Math.floor(time / 60);
       const seconds = Math.floor(time - Math.floor(time / 60) * 60)
         .toString()
-        .padStart(2, "0");
+        .padStart(2, '0');
       songtime = 0;
-      const duration = minutes + ":" + seconds;
-      localStorage.setItem("songMinutes", minutes);
-      localStorage.setItem("songSeconds", seconds);
-      document.querySelector(".playerDuration").innerHTML = `${duration}`;
+      const duration = minutes + ':' + seconds;
+      localStorage.setItem('songMinutes', minutes);
+      localStorage.setItem('songSeconds', seconds);
+      document.querySelector('.playerDuration').innerHTML = `${duration}`;
     });
 }
-window.onload = player(localStorage.getItem("lastSong"));
+window.onload = player(localStorage.getItem('lastSong'));
 
-const songStart = document.getElementById("start");
-const songPause = document.getElementById("pause");
+const songStart = document.getElementById('start');
+const songPause = document.getElementById('pause');
 let timer;
 let songtime = 0;
-songStart.addEventListener("click", function () {
-  songStart.classList.toggle("d-none");
-  songPause.classList.toggle("d-none");
-  const songduration = localStorage.getItem("songMinutes") + ":" + localStorage.getItem("songSeconds");
-  const totDuration = Math.floor(localStorage.getItem("songMinutes") * 60 + localStorage.getItem("songSeconds") * 1);
+songStart.addEventListener('click', function () {
+  songStart.classList.toggle('d-none');
+  songPause.classList.toggle('d-none');
+  const songduration = localStorage.getItem('songMinutes') + ':' + localStorage.getItem('songSeconds');
+  const totDuration = Math.floor(localStorage.getItem('songMinutes') * 60 + localStorage.getItem('songSeconds') * 1);
   timer = setInterval(function () {
     const minutes = Math.floor(songtime / 60);
     const seconds = Math.floor(songtime - Math.floor(songtime / 60) * 60)
       .toString()
-      .padStart(2, "0");
-    const duration = minutes + ":" + seconds;
-    document.querySelector(".playerTimer").innerHTML = duration;
+      .padStart(2, '0');
+    const duration = minutes + ':' + seconds;
+    document.querySelector('.playerTimer').innerHTML = duration;
     const percentage = ((songtime / totDuration) * 100).toFixed(1);
     console.log(percentage);
-    document.querySelector(".progress-bar").style.width = `${percentage}%`;
+    document.querySelector('.progress-bar').style.width = `${percentage}%`;
     if (songduration === duration) {
       clearInterval(timer);
       song.play();
-      songStart.classList.toggle("d-none");
-      songPause.classList.toggle("d-none");
+      songStart.classList.toggle('d-none');
+      songPause.classList.toggle('d-none');
       songtime = 0;
-      console.log("fin song");
+      console.log('fin song');
     } else {
       songtime++;
     }
   }, 1000);
 });
-songPause.addEventListener("click", function () {
+songPause.addEventListener('click', function () {
   clearInterval(timer);
   song.pause();
-  songStart.classList.toggle("d-none");
-  songPause.classList.toggle("d-none");
+  songStart.classList.toggle('d-none');
+  songPause.classList.toggle('d-none');
 });
+
+//like button album page fn
+
+const likeButtonClick = document.getElementById('likeButtonClick');
+const likeButtonClickNone = document.getElementById('likeButtonClickNone');
+console.log(likeButtonClick.target);
+console.log(likeButtonClickNone.target);
+
+likeButtonClick.addEventListener('click', () => {
+  likeButtonClick.classList.toggle('d-none');
+  likeButtonClickNone.classList.toggle('d-none');
+});
+
+likeButtonClickNone.addEventListener('click', () => {
+  likeButtonClick.classList.toggle('d-none');
+  likeButtonClickNone.classList.toggle('d-none');
+});
+
+//likeButtonClickNone.onclick = likeButton;
