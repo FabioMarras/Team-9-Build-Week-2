ca534e17c8872bb6c96c5d4ea6eb0cec79156eab
Bilgen Kaan Remzi <137759282+Rekabil@users.noreply.github.com>
2023-08-31 12:58:29 +0200

    mobile

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -15,20 +15,12 @@ function expandRight(event) {
 }
 
 function collapseLeft(event) {
-  const disp = document.querySelector(".sidebarLeft").querySelectorAll("span");
-  disp.forEach((element) => {
-    element.style.display = "none";
-  });
   document.querySelector(".sidebarLeft").classList.toggle("collapsed");
   document.querySelector(".expand-left").classList.toggle("d-none");
   document.querySelector(".collapse-left").classList.toggle("d-none");
   document.getElementById("main").classList.toggle("pSidebarleft");
 }
 function expandLeft(event) {
-  const disp = document.querySelector(".sidebarLeft").querySelectorAll("span");
-  disp.forEach((element) => {
-    element.style.display = "inline";
-  });
   document.querySelector(".expand-left").classList.toggle("d-none");
   document.querySelector(".collapse-left").classList.toggle("d-none");
   document.querySelector(".sidebarLeft").classList.toggle("collapsed");
